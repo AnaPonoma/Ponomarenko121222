94a7df61b70455e0f7abe364a1d2ccb449ae42e2
AnaPonoma <116743637+AnaPonoma@users.noreply.github.com>
2023-01-12 14:36:01 +0100

    HW4task21 fix comments by lector

--- src/ProjectAQA/HW4/HW4task21.java
+++ src/ProjectAQA/HW4/HW4task21.java
@@ -9,34 +9,34 @@ public class HW4task21 {
 
 
     }
+
     public static String prntMonth(int month) {
-        if (month ==1)
-           return ("Jan");
-        else if (month ==2)
+        if (month == 1) {
+            return ("Jan");
+        } else if (month == 2) {
             return ("Feb");
-        else if (month ==2)
-            return ("Feb");
-        else if (month ==3)
+        } else if (month == 3) {
             return ("Mar");
-        else if (month ==4)
+        } else if (month == 4) {
             return ("Apr");
-        else if (month ==5)
+        } else if (month == 5) {
             return ("May");
-        else if (month ==6)
+        } else if (month == 6) {
             return ("Jun");
-        else if (month ==7)
+        } else if (month == 7) {
             return ("Jul");
-        else if (month ==8)
+        } else if (month == 8) {
             return ("Aug");
-        else if (month ==9)
+        } else if (month == 9) {
             return ("Sept");
-        else if (month ==10)
+        } else if (month == 10) {
             return ("Oct");
-        else if (month ==11)
+        } else if (month == 11) {
             return ("Nov");
-        else if (month ==12)
+        } else if (month == 12) {
             return ("Dec");
-return "Not a valid month";
-}
+        }
+        return "Not a valid month";
+    }
 
 }
